6b3caa22b8f3a5815ac9fa7e73f2da83808fc7c7
tennismaster <wjdalsgh834@naver.com>
2018-06-06 18:22:24 +0900

    리뷰 구메 페이지

--- routes/board.js
+++ routes/board.js
@@ -29,7 +29,7 @@ router.get('/sell/:item_name', function(req, res, next) {
 router.get('/index', function(req, res, next) {
   pool.getConnection(function(err, connection){
  
-   connection.query('SELECT * From product', function(err,rows){
+   connection.query('SELECT * From board', function(err,rows){
      if(err) console.error("err: "+err);
  
      res.render('index', { title: 'test',rows: rows });
@@ -40,7 +40,7 @@ router.get('/index', function(req, res, next) {
   
  });
 
-router.get('/menu/:item_name',function(req,res,next){
+router.get('/tshirt_menu/:item_name',function(req,res,next){
 
   var item_name=req.params.item_name;
 
@@ -61,7 +61,7 @@ router.get('/menu/:item_name',function(req,res,next){
       prod_rev=rows;
       console.log("rev : ",prod_rev);
       
-      res.render('product', { title: 'test', rows: prod_info, db: prod_rev });//여기에 함부로 추가면 좇된다. 이대로 형식을 둡니다. 
+      res.render('tshirt', { title: 'test', rows: prod_info, db: prod_rev });//여기에 함부로 추가면 좇된다. 이대로 형식을 둡니다. 
 
       connection.release();
     });
@@ -78,7 +78,7 @@ router.get('/menu/:item_name',function(req,res,next){
     pool.getConnection(function(err, connection){
    
   
-       var sql="select item_name,item_type,price,color,spec,sell from product where item_name=? ";
+       var sql="select item_name,item_type,price,color,spec from product where item_name=? ";
   // list js 만들 떄 사용하는건데 이렇게 해도 되는건가.
       connection.query(sql,[item_name], function(err,rows){
         if(err) console.error("err: "+err);
@@ -91,16 +91,14 @@ router.get('/menu/:item_name',function(req,res,next){
       });
      });
     });
-  router.get('/menu/:item_type',function(req,res,next){
-
-    var item_type = req.params.item_type;
+  router.get('/tshirt_menu',function(req,res,next){
     pool.getConnection(function(err, connection){
-      var sql="select item_name,item_type,price,color,spec,sell from product where item_type=? ";
-      connection.query(sql,[item_type], function(err,rows){
+   
+      connection.query('SELECT idx,item_name,item_type,price,color,spec From product', function(err,rows){
         if(err) console.error("err: "+err);
         
     
-        res.render('menu', { title: 'test',rows: rows });
+        res.render('tshirt_menu', { title: 'test',rows: rows });
         connection.release();
     
       });
