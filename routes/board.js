6e1c148260dfc0074c45b474419bdc14beb6026b
tennismaster <wjdalsgh834@naver.com>
2018-06-06 18:06:42 +0900

    리뷰, 구매페이지 잘됨

--- routes/board.js
+++ routes/board.js
@@ -6,21 +6,31 @@ var pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   database: 'test',
-  password: 'dmlgjs'
+  password: '940306'
 });
 
 
-
-
-
 /* GET home page. */
-
+router.get('/sell/:item_name', function(req, res, next) {
+  var item_name = req.params.item_name;
+  console.log("????" + item_name);
+  pool.getConnection(function(err, connection){
+    var sql = 'UPDATE product SET sell = sell + 1 WHERE item_name = ?'
+    connection.query(sql, [item_name], function(err,rows){
+      if(err) console.error("err: "+err);
+  
+      //res.render('index', { title: 'test',rows: rows });
+      connection.release();
+  
+    });
+  });
+  res.redirect('/board/index');
+});
 router.get('/index', function(req, res, next) {
   pool.getConnection(function(err, connection){
  
-   connection.query('SELECT * From board', function(err,rows){
+   connection.query('SELECT * From product', function(err,rows){
      if(err) console.error("err: "+err);
-     console.log("rows : "+ JSON.stringify(rows));
  
      res.render('index', { title: 'test',rows: rows });
      connection.release();
@@ -30,38 +40,73 @@ router.get('/index', function(req, res, next) {
   
  });
 
-router.get('/tshirt_menu/:item_name',function(req,res,next){
+router.get('/menu/:item_name',function(req,res,next){
 
   var item_name=req.params.item_name;
 
   pool.getConnection(function(err, connection){
- 
-
-     var sql="select idx,item_name,item_type,price,color,spec from product where item_name=? ";
+    var sql1="select idx,item_name,item_type,price,color,spec from product where item_name=? ";
+    var sql2 = "select * from review where item_name=?"
+    var prod_info=[];
+    var prod_rev=[];
 // list js 만들 떄 사용하는건데 이렇게 해도 되는건가.
-    connection.query(sql,[item_name], function(err,rows){
+    connection.query(sql1, item_name, function(err,rows){
       if(err) console.error("err: "+err);
-      console.log("rows : ",rows);
-  
-      res.render('tshirt', { title: 'test',rows: rows });//여기에 함부로 추가면 좇된다. 이대로 형식을 둡니다. 
+      prod_info=rows;
+      console.log("info : ",prod_info);
+    });
+
+    connection.query(sql2, item_name, function(err,rows){
+      if(err) console.error("err: "+err);
+      prod_rev=rows;
+      console.log("rev : ",prod_rev);
+      
+      res.render('product', { title: 'test', rows: prod_info, db: prod_rev });//여기에 함부로 추가면 좇된다. 이대로 형식을 둡니다. 
 
       connection.release();
-  
     });
-   });
-  });
 
-  router.get('/tshirt_menu',function(req,res,next){
+   });  
+});
+ 
+
+  router.get('/buy/:item_name',function(req,res,next){
+
+    var item_name = req.params.item_name;
+    var select_size = req.query.select_size;
+  
     pool.getConnection(function(err, connection){
    
-      connection.query('SELECT idx,item_name,item_type,price,color,spec From product', function(err,rows){
+  
+       var sql="select item_name,item_type,price,color,spec,sell from product where item_name=? ";
+  // list js 만들 떄 사용하는건데 이렇게 해도 되는건가.
+      connection.query(sql,[item_name], function(err,rows){
         if(err) console.error("err: "+err);
-        console.log("rows : "+ JSON.stringify(rows));
+        
     
-        res.render('tshirt_menu', { title: 'test',rows: rows });
+        res.render('buy', { title: 'test',rows: rows, select_size: select_size });//여기에 함부로 추가면 좇된다. 이대로 형식을 둡니다. 
+  
         connection.release();
     
       });
      });
     });
+  router.get('/menu/:item_type',function(req,res,next){
+
+    var item_type = req.params.item_type;
+    pool.getConnection(function(err, connection){
+      var sql="select item_name,item_type,price,color,spec,sell from product where item_type=? ";
+      connection.query(sql,[item_type], function(err,rows){
+        if(err) console.error("err: "+err);
+        
+    
+        res.render('menu', { title: 'test',rows: rows });
+        connection.release();
+    
+      });
+     });
+    });
+
+
+    
 module.exports = router;
